9de9357a03672800bebd033c72853f3d9787cc8f
LinkedList <martin.macko@productboard.com>
2022-12-08 16:27:36 +0100

    Day 6 - part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::env;
-use itertools::Itertools;
+use std::collections::HashSet;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,14 +11,15 @@ fn main() {
     let linesReader = read_lines(path).expect("We have a no go reading lines");
     let linesVec = linesReader.map(|r| r.unwrap()).collect::<Vec<String>>();
     let line = &linesVec[0];
-    let iter = line.chars().tuple_windows::<(_, _, _, _)>();
 
-    for (num, (a, b, c, d)) in iter.enumerate() {
-        if(a != b && a != c && a != d && 
-           b != c && b != d &&
-           c != d) {
-            println!("{}{}{}{}", a, b, c, d);
-            println!("{:?}", num + 4);
+    for (num, ch) in line.chars().enumerate() {
+        let mut set = HashSet::<char>::new();
+        let slice = &line[num..(num+14)];
+        for ch in slice.chars() {
+            set.insert(ch);
+        }
+        if set.len() == 14 {
+            println!("{:?}", num + 14);
             break;
         }
     }
